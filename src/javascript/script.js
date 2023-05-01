6190cf28379194b960bb17cb92ed409f54fe88b3
jonas souza <jonassouza305@yahoo.com.br>
2023-05-01 10:10:00 -0300

    modifiquei meu codigo javascript usando forEach no lugar de map e refatorei

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,27 +1,22 @@
-const campoFormulario = [...document.querySelectorAll('.conteudo')]
-const campoObrigatorio = [...document.querySelectorAll("label[class='aviso']")]
-const botao = document.getElementById('btm')
+const campoFormulario = document.querySelectorAll('.conteudo')
+const campoObrigatorio = document.querySelector("label[class='aviso']")
+const botao = document.querySelector('#btm')
 
-
-botao.addEventListener('click', () => {
-    campoFormulario.map((el) => {
-        if (el.value === "") {
-            el.classList.add('bordaVermelho')
-            campoObrigatorio.map((ele) => {
-                ele.innerHTML = 'campo obrigatorio'
-            })
+botao.addEventListener('click', (el) => {
+    el.preventDefault()
+    validarCampos()
+})
+function validarCampos() {
+    campoFormulario.forEach((input) => {
+        if (input.value) {
+            input.classList.remove('bordaVermelho')
+            input.classList.add('bordaVerde')
+            input.nextElementSibling.classList.remove('aviso')
         } else {
-            adicionarEremoverBordas()
-            campoObrigatorio.map((ele) => {
-                ele.innerHTML = ''
-
-            })
-        }
-
-        function adicionarEremoverBordas() {
-            el.classList.remove('bordaVermelho')
-            el.classList.add('bordaVerde')
+            input.classList.remove('bordaVerde')
+            input.classList.add('bordaVermelho')
+            input.nextElementSibling.classList.add('aviso')
         }
     })
-})
+}
 
